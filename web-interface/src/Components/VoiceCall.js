b110a3773b762b1eb93712177d97013fb1403b19
Viet Thai Nguyen <vietthai.nguyen03@gmail.com>
2024-07-13 00:44:44 +1000

    [web-interface] feat: Call Management
    This commit:
    Add a new feature that record the incoming voice
    on the other side every 5 seconds and send it
    to the server for analyse to see if the voice
    is deepfake or not.
    
    Add a new feature that allows the user to switch
    between the audio input types such as microphone
    and audio file to test the case where the attacker
    or the deepfake user, initiate the call.
    
    Add a new feature that allows the user to answer
    the call with an audio file to test the case where
    the attacker or the deepfake user answer the call.
    
    Add a new feature that force one person to hangup
    if the other person hangup to resemble a phone
    call.

--- web-interface/src/Components/VoiceCall.js
+++ web-interface/src/Components/VoiceCall.js
@@ -6,54 +6,59 @@ const VoiceCall = ({clientID, webSocket, setWebSocket, appid}) => {
   const [mediaRecorder, setMediaRecorder] = useState(null);
   const [stream, setStream] = useState(null);
   const [intervalID, setIntervalID] = useState(null);
-
+  const [rtcClient, setRtcClient] = useState(null);
   const [audioTracks, setAudioTracks] = useState({
     localAudioTrack: null,
     remoteAudioTracks: []
   });
-
-  const [rtcClient, setRtcClient] = useState(null);
-
-  /*let audioTracks = {
-    localAudioTrack: null,
-    remoteAudioTracks: {}
-  }
-
-  //let rtcClient;
-
-  const handleUserJoined = async (user) => {
-    console.log('A New User Has Joined');
-    console.log(user)
-  }
-
-  const handleUserPublished = async (user, mediaType) => {
-    console.log(rtcClient);
-    await rtcClient.subscribe(user, mediaType);
-
-    if (mediaType === 'audio') {
-      audioTracks.remoteAudioTracks[user.uid] = [user.audioTrack];
-      user.audioTrack.play();
-    }
-  }*/
+  const [audioInputType, setAudioInputType] = useState('mic'); //file
 
   useEffect(() => {
     const startRecording = () => {
       if (intervalID === null) {
         if (isRecording === true) {
           setIntervalID(setInterval(() => {
-            mediaRecorder.stop();
-            mediaRecorder.start();
+            if (mediaRecorder) {
+              mediaRecorder.stop();
+            }
+            if (mediaRecorder) {
+              mediaRecorder.start();
+            }
           },5000))
         }
       }
     }
-    startRecording();
-  }, [mediaRecorder])
+
+    if (isRecording === false) {
+      if (intervalID !== null) {
+        clearInterval(intervalID);
+        setIntervalID(null);
+
+        if (mediaRecorder) {
+          console.log('Rem');
+          mediaRecorder.stop();
+        }
+
+        if (stream) {
+          stream.getTracks().forEach(track => track.stop());
+          setStream(null);
+        }
+
+        audioTracks.localAudioTrack.stop();
+        audioTracks.localAudioTrack.close();
+        rtcClient.unpublish();
+        rtcClient.leave();
+      }
+    }
+
+    if (isRecording === true) {
+      startRecording();
+    }
+  }, [isRecording]);
 
   const recordAudioStream = (audioTrack) => {
     const mediaRecorder = new MediaRecorder(new MediaStream([audioTrack]));
 
-    let chunks = [];
     mediaRecorder.ondataavailable = (e) => {
       if (webSocket && webSocket.readyState === WebSocket.OPEN) {
         webSocket.send(e.data);
@@ -61,7 +66,7 @@ const VoiceCall = ({clientID, webSocket, setWebSocket, appid}) => {
     };
   
     mediaRecorder.onstop = () => {
-      //const audioBlob = new Blob(chunks, { type: 'audio/wav' });
+      //Place Holder
     };
   
     mediaRecorder.start();
@@ -75,15 +80,20 @@ const VoiceCall = ({clientID, webSocket, setWebSocket, appid}) => {
       const rtcClient = AgoraRTC.createClient({mode:'rtc', codec:'vp8'});
 
       const handleUserJoined = async (user) => {
-        console.log('A New User Has Joined');
-        console.log(user)
+        //Place Holder
       }
     
       const handleUserPublished = async (user, mediaType) => {
         await rtcClient.subscribe(user, mediaType);
     
         if (mediaType === 'audio') {
-          audioTracks.remoteAudioTracks[user.uid] = [user.audioTrack];
+          let temp = audioTracks.remoteAudioTracks;
+          temp[user.uid] = [user.audioTrack];
+
+          setAudioTracks(prevState => ({
+            ...prevState,
+            remoteAudioTracks: temp
+          }));
 
           user.audioTrack.play();
           recordAudioStream(user.audioTrack.mediaStreamTrack);
@@ -91,7 +101,9 @@ const VoiceCall = ({clientID, webSocket, setWebSocket, appid}) => {
       }
 
       let handleUserLeft = async (user) => {
-        delete audioTracks.remoteAudioTracks[user.uid]
+        delete audioTracks.remoteAudioTracks[user.uid];
+    
+        setIsRecording(false);
       }
   
       rtcClient.on('user-joined', handleUserJoined);
@@ -109,11 +121,6 @@ const VoiceCall = ({clientID, webSocket, setWebSocket, appid}) => {
   }, [audioTracks.localAudioTrack]);
 
   let initRtc = async () => {
-    /*rtcClient = AgoraRTC.createClient({mode:'rtc', codec:'vp8'});
-  
-    rtcClient.on('user-joined', handleUserJoined);
-    rtcClient.on('user-published', handleUserPublished);*/
-
     await rtcClient.join(appid, 'main', null, clientID);
 
     const localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
@@ -121,32 +128,57 @@ const VoiceCall = ({clientID, webSocket, setWebSocket, appid}) => {
       ...prevState,
       localAudioTrack: localAudioTrack
     }));
-
-    //audioTracks.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
-    //await rtcClient.publish(audioTracks.localAudioTrack);
   }
 
   let leaveRoom = async () => {
+    setIsRecording(false);
+  }
+
+  const handleInputSwitchToAudioFile = async () => {
+    const fileAudioTrack = await AgoraRTC.createBufferSourceAudioTrack({
+      source: '/audiofiles/real.m4a'
+    });
+
+    fileAudioTrack.startProcessAudioBuffer();
+
     audioTracks.localAudioTrack.stop();
     audioTracks.localAudioTrack.close();
-    rtcClient.unpublish();
-    rtcClient.leave();
 
-    if (intervalID !== null) {
-      clearInterval(intervalID);
-      setIntervalID(null);
-    }
+    setAudioTracks(prevState => ({
+      ...prevState,
+      localAudioTrack: fileAudioTrack
+    }));
 
-    if (mediaRecorder) {
-      mediaRecorder.stop();
-    }
+    setAudioInputType('file');
+  }
 
-    if (stream) {
-        stream.getTracks().forEach(track => track.stop());
-        setStream(null);
-    }
+  const handleInputSwitchToMicrophone = async () => {
+    audioTracks.localAudioTrack.stopProcessAudioBuffer();
+    audioTracks.localAudioTrack.stop();
+    audioTracks.localAudioTrack.close();
 
-    setIsRecording(false);
+    const localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+    setAudioTracks(prevState => ({
+      ...prevState,
+      localAudioTrack: localAudioTrack
+    }));
+
+    setAudioInputType('mic');
+  }
+
+  const handleAnswerWithAudioFile = async () => {
+    await rtcClient.join(appid, 'main', null, clientID);
+
+    const fileAudioTrack = await AgoraRTC.createBufferSourceAudioTrack({
+      source: '/audiofiles/nier.mp3'
+    });
+
+    fileAudioTrack.startProcessAudioBuffer();
+
+    setAudioTracks(prevState => ({
+      ...prevState,
+      localAudioTrack: fileAudioTrack
+    }));
   }
 
   const handleLogout = () => {
@@ -162,6 +194,18 @@ const VoiceCall = ({clientID, webSocket, setWebSocket, appid}) => {
         <div>
             <button onClick={leaveRoom} disabled={!isRecording}>Hangup Call</button>
         </div>
+        <br/>
+        <div>
+          <button onClick={handleInputSwitchToAudioFile} disabled={!isRecording || audioInputType === 'file'}>Switch Input To Audio File</button>
+        </div>
+        <div>
+          <button onClick={handleInputSwitchToMicrophone} disabled={!isRecording || audioInputType === 'mic'}>Switch Input To Microphone</button>
+        </div>
+        <br/>
+        <div>
+          <button onClick={handleAnswerWithAudioFile} disabled={isRecording}>Answer Call With Audio File</button>
+        </div>
+        <br/>
         <div>
             <button onClick={handleLogout} disabled={isRecording}>Logout</button>
         </div>
